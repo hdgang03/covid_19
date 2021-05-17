25a5bc03800e6fda08f7c14666d6fe59b13779f7
Hites Dániel <hitesdani@gmail.com>
2021-05-17 19:34:32 +0200

    added star, bomb

--- game.js
+++ game.js
@@ -8,7 +8,9 @@ function main() {
     const lifeCounter = document.querySelector('#life');
     const scoreCounter = document.querySelector('#currentscore');
     const highScoreCounter = document.querySelector("#highscore");
+    const surprises = document.querySelector("#surprises");
 
+    let point = 100;
     let life;
     let score;
     let highscore = localStorage.getItem("highscore");
@@ -16,9 +18,14 @@ function main() {
         localStorage.setItem("highscore", 0);
     }
     highScoreCounter.innerText= highscore;
-
+    let covidInterval = 2100;
+    let heartInterval = 16000;
+    let bombInterval = 22000;
+    let starInterval = 33000;
     let covidCreationInterval;
     let heartCreationInterval;
+    let bombCreationInterval;
+    let starCreationInterval;
 
 
     startButton.addEventListener("click", (event) => {
@@ -38,6 +45,27 @@ function main() {
         if (life <= 0) {
             gameOver();
         }
+    }
+
+    function handleStar () {
+        point = 200;
+        surprises.innerText = "Double points!";
+        setTimeout(function(){ surprises.innerText = "Double points ending!"; }, 12000);
+        setTimeout(function(){ point = 100; surprises.innerText = ""; }, 15000);
+    }
+
+    function handleBomb () {
+        surprises.innerText = "BANG";
+        setTimeout(function(){ surprises.innerText = ""; }, 300);
+        let covids = document.querySelectorAll(".covid");
+        covids.forEach(covid => {
+            covid.src = "img/explosion.png";
+            covid.classList.remove("covid");
+            covid.classList.add("dead-covid");
+            score += point;
+            scoreCounter.innerText = score;
+            handleHighscore();
+        });
     }
 
     function letDoctorMove(event) {
@@ -107,7 +135,8 @@ function main() {
                     covid.src = "img/explosion.png";
                     covid.classList.remove("covid");
                     covid.classList.add("dead-covid");
-                    score += 100;
+                    syringe.remove();
+                    score += point;
                     scoreCounter.innerText = score;
                     handleHighscore();
                 }
@@ -117,8 +146,27 @@ function main() {
                 if(checkSyringeCollision(syringe, heart)) {
                     heart.classList.remove("heart");
                     heart.classList.add("dead-heart");
+                    syringe.remove();
                     life++;
                     lifeCounter.innerText = life;
+                }
+            })
+            let bombs = document.querySelectorAll(".bomb");
+            bombs.forEach(bomb => {
+                if(checkSyringeCollision(syringe, bomb)) {
+                    bomb.classList.remove("bomb");
+                    bomb.classList.add("dead-bomb");
+                    syringe.remove();
+                    handleBomb();
+                }
+            })
+            let stars = document.querySelectorAll(".star");
+            stars.forEach(star => {
+                if(checkSyringeCollision(syringe, star)) {
+                    star.classList.remove("star");
+                    star.classList.add("dead-star");
+                    syringe.remove();
+                    handleStar();
                 }
             })
             if (topPosition < 20) {
@@ -139,6 +187,8 @@ function main() {
         newHeart.style.left = `${Math.floor(Math.random() * 330) + 30}px`;
         playArea.appendChild(newHeart);
         moveHeart(newHeart);
+        heartInterval = Math.floor(Math.random()*24000) + 14000;
+       // console.log(heartInterval);
     }
 
 
@@ -152,6 +202,34 @@ function main() {
         newCovid.style.left = `${Math.floor(Math.random() * 330) + 30}px`;
         playArea.appendChild(newCovid);
         moveCovid(newCovid);
+        covidInterval = Math.floor(Math.random()*2600) + 1900;
+        // console.log(covidInterval);
+    }
+
+    function createBomb() {
+        let newBomb = document.createElement('img');
+        newBomb.src = 'img/bomb.png';
+        newBomb.classList.add('bomb');
+        newBomb.classList.add('bomb-transition');
+        newBomb.style.top = '50px';
+        newBomb.style.left = `${Math.floor(Math.random() * 330) + 30}px`;
+        playArea.appendChild(newBomb);
+        moveBomb(newBomb);
+        bombInterval = Math.floor(Math.random()*20000) + 30000;
+        // console.log(bombInterval);
+    }
+
+    function createStar() {
+        let newStar = document.createElement('img');
+        newStar.src = 'img/star.png';
+        newStar.classList.add('star');
+        newStar.classList.add('star-transition');
+        newStar.style.top = '50px';
+        newStar.style.left = `${Math.floor(Math.random() * 330) + 30}px`;
+        playArea.appendChild(newStar);
+        moveStar(newStar);
+        starInterval = Math.floor(Math.random()*30000) + 45000;
+        // console.log(starInterval);
     }
 
 
@@ -172,6 +250,7 @@ function main() {
             }
         }, 30)
     }
+
     function moveHeart(heartElem) {
         let moveHeartdInterval = setInterval(() => {
             let xPosition = parseInt(window.getComputedStyle(heartElem).getPropertyValue('top'));
@@ -185,6 +264,40 @@ function main() {
                 }
             } else {
                 heartElem.style.top = `${xPosition + 2}px`;
+            }
+        }, 30)
+    }
+
+    function moveBomb(bombElem) {
+        let moveBombInterval = setInterval(() => {
+            let xPosition = parseInt(window.getComputedStyle(bombElem).getPropertyValue('top'));
+            if (xPosition >= 700) {
+                if (Array.from(bombElem.classList).includes("dead-bomb")) {
+                    bombElem.remove();
+                    clearInterval(moveBombInterval)
+                } else {
+                    bombElem.remove();
+                    clearInterval(moveBombInterval)
+                }
+            } else {
+                bombElem.style.top = `${xPosition + 2}px`;
+            }
+        }, 30)
+    }
+
+    function moveStar(starElem) {
+        let moveStarInterval = setInterval(() => {
+            let xPosition = parseInt(window.getComputedStyle(starElem).getPropertyValue('top'));
+            if (xPosition >= 700) {
+                if (Array.from(starElem.classList).includes("dead-star")) {
+                    starElem.remove();
+                    clearInterval(moveStarInterval)
+                } else {
+                    starElem.remove();
+                    clearInterval(moveStarInterval)
+                }
+            } else {
+                starElem.style.top = `${xPosition + 2}px`;
             }
         }, 30)
     }
@@ -237,8 +350,10 @@ function main() {
         startButton.style.display = 'none';
         instructions.style.display = 'none';
         window.addEventListener("keydown", letDoctorMove);
-        covidCreationInterval = setInterval(function () { createCovid() }, 2100);
-        heartCreationInterval = setInterval(function () {createHeart()}, 16000);
+        covidCreationInterval = setInterval(function () { createCovid() }, covidInterval);
+        heartCreationInterval = setInterval(function () {createHeart()}, heartInterval);
+        bombCreationInterval = setInterval(function () {createBomb()}, bombInterval);
+        starCreationInterval = setInterval(function () {createStar()}, starInterval);
     }
 }
 window.addEventListener("load", main);
