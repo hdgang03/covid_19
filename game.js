4e9b4c84740a2f9d1fd0ba98cb7d51e2b81026fb
Hites Dániel <hitesdani@gmail.com>
2021-05-18 21:24:57 +0200

    refactored functions, improved shooting accuracy, added giant covid

--- game.js
+++ game.js
@@ -10,106 +10,52 @@ function main() {
     const highScoreCounter = document.querySelector("#highscore");
     const surprises = document.querySelector("#surprises");
 
+    //VARIABLES
+    let health = 500;
     let point = 100;
     let life;
     let score;
+    let giantFallSpeed = 1;
     let fallSpeed = 2;
+
+    //HIGHSCORE
     let highscore = localStorage.getItem("highscore");
     if (highscore === null) {
         localStorage.setItem("highscore", 0);
     }
     highScoreCounter.innerText= highscore;
+
+    //INTERVALS 
     let covidInterval = 2100;
     let heartInterval = 16000;
     let bombInterval = 22000;
     let starInterval = 33000;
+    let giantCovidInterval = 58000;
     let covidCreationInterval;
     let heartCreationInterval;
     let bombCreationInterval;
     let starCreationInterval;
+    let giantCovidCreationInterval;
 
 
     startButton.addEventListener("click", (event) => {
         playGame()
     });
 
-    function resumeGame (/*event*/) {
-       /* if(event.key === "Escape") {
-            startButton.style.display = 'none';
-            instructions.style.display = 'none';
-            window.addEventListener("keydown", letDoctorMove);
-            fallSpeed = 2;
-            covidCreationInterval = setInterval(function () { createCovid() }, covidInterval);
-            heartCreationInterval = setInterval(function () {createHeart()}, heartInterval);
-            bombCreationInterval = setInterval(function () {createBomb()}, bombInterval);
-            starCreationInterval = setInterval(function () {createStar()}, starInterval);
-            window.removeEventListener("keydown", resumeGame);
-        }*/
 
-        startButton.style.display = 'none';
-        instructions.style.display = 'none';
-        window.addEventListener("keydown", letDoctorMove);
-        fallSpeed = 2;
-        covidCreationInterval = setInterval(function () { createCovid() }, covidInterval);
-        heartCreationInterval = setInterval(function () {createHeart()}, heartInterval);
-        bombCreationInterval = setInterval(function () {createBomb()}, bombInterval);
-        starCreationInterval = setInterval(function () {createStar()}, starInterval);
-        window.removeEventListener("keydown", resumeGame);
-
-    }
-
-    function endGame () {
-        window.removeEventListener("keydown", letDoctorMove);
-        startButton.style.display = 'none';
-        clearInterval(covidCreationInterval);
-        clearInterval(heartCreationInterval);
-        clearInterval(bombCreationInterval);
-        clearInterval(starCreationInterval);
-        let covids = document.querySelectorAll(".covid");
-        covids.forEach(covid => covid.remove());
-        let syringes = document.querySelectorAll(".syringe");
-        syringes.forEach(syringe => syringe.remove());
-        let hearts = document.querySelectorAll(".heart");
-        hearts.forEach(heart => heart.remove());
-        let stars = document.querySelectorAll(".star");
-        stars.forEach(star => star.remove());
-        let bombs = document.querySelectorAll(".bomb");
-        bombs.forEach(bomb => bomb.remove());
-        handleHighscore();
-        instructions.innerHTML = `Game Over! Your final score is ${scoreCounter.innerText}!`;
-        instructions.style.display = "block";
-        instructions.style.color = "red";
-        startButton.style.display = "block";
-    }
-
-    function pauseGame (event) {
-        if (event.key === "Escape") {
-            event.preventDefault();
-            window.removeEventListener("keydown", letDoctorMove);
-            clearInterval(covidCreationInterval);
-            clearInterval(heartCreationInterval);
-            clearInterval(bombCreationInterval);
-            clearInterval(starCreationInterval);
-            fallSpeed = 0;
-            let syringes = document.querySelectorAll(".syringe");
-            syringes.forEach(syringe => syringe.remove());
-            instructions.innerHTML = `Paused`;
-            instructions.style.display = "block";
-            instructions.style.color = "red";
-            instructions.insertAdjacentHTML("beforeend", `<button id="resume">Resume</button>` );      
-            instructions.insertAdjacentHTML("beforeend", `<button id="end">End</button>` );
-            const resumeBtn = document.querySelector("#resume");   
-            const endBtn = document.querySelector("#end");
-            resumeBtn.addEventListener('click', resumeGame);   
-            endBtn.addEventListener('click', endGame);
-           /* window.addEventListener("keydown", resumeGame);*/
-        }
-    }
-
+    //HANDLE EVENTS
     function handleHighscore () {
         if (highscore < score) {
             localStorage.setItem("highscore", score);
             highScoreCounter.innerText= score;
+        }
+    }
+
+    function handleGiantCollision () {
+        life -=10;
+        lifeCounter.innerText = life;
+        if (life <= 0) {
+            gameOver();
         }
     }
 
@@ -142,6 +88,7 @@ function main() {
         });
     }
 
+    //DOCTOR MOVE
     function letDoctorMove(event) {
         if (event.key === "ArrowLeft") {
             event.preventDefault();
@@ -155,7 +102,6 @@ function main() {
         }
     }
 
-
     function moveLeft() {
         let leftPosition = window.getComputedStyle(shooter).getPropertyValue('left');
         if (shooter.style.left === "0px") {
@@ -166,7 +112,6 @@ function main() {
             shooter.style.left = `${position}px`;
         }
     }
-
 
     function moveRight() {
         let leftPosition = window.getComputedStyle(shooter).getPropertyValue('left');
@@ -180,13 +125,12 @@ function main() {
         }
     }
 
-
+    //SYRINGE
     function fireSyringe() {
         let syringe = createSyringeElement();
         playArea.appendChild(syringe);
         moveSyringe(syringe);
     }
-
 
     function createSyringeElement() {
         let xPosition = parseInt(window.getComputedStyle(shooter).getPropertyValue('left'));
@@ -198,7 +142,6 @@ function main() {
         newSyringe.style.top = `${yPosition - 10}px`;
         return newSyringe;
     }
-
 
     function moveSyringe(syringe) {
         let syringeInterval = setInterval(() => {
@@ -243,6 +186,21 @@ function main() {
                     handleStar();
                 }
             })
+            let giantCovids = document.querySelectorAll(".giant-covid");
+            giantCovids.forEach(giant => {
+                if(checkSyringeCollisionGiant(syringe, giant)) {
+                    health -=3;
+                    syringe.remove();                    
+                    if (health <= 0) {
+                        giant.src = "img/explosion.png";
+                        giant.classList.remove("giant-covid");
+                        giant.classList.add("dead-covid");
+                        syringe.remove();
+                        score += 5*point;
+                        health = 500;
+                    }
+                }
+            })
             if (topPosition < 20) {
                 syringe.remove();
                 clearInterval(syringeInterval);
@@ -252,20 +210,7 @@ function main() {
         }, 10)
     }
 
-    function createHeart() {
-        let newHeart = document.createElement('img');
-        newHeart.src = 'img/heart.png';
-        newHeart.classList.add('heart');
-        newHeart.classList.add('heart-transition');
-        newHeart.style.top = '50px';
-        newHeart.style.left = `${Math.floor(Math.random() * 330) + 30}px`;
-        playArea.appendChild(newHeart);
-        moveHeart(newHeart);
-        heartInterval = Math.floor(Math.random()*24000) + 14000;
-       // console.log(heartInterval);
-    }
-
-
+    //COVID
     function createCovid() {
         let newCovid = document.createElement('img');
         let covidSpriteImg = covidImgs[Math.floor(Math.random() * covidImgs.length)];
@@ -276,36 +221,7 @@ function main() {
         newCovid.style.left = `${Math.floor(Math.random() * 330) + 30}px`;
         playArea.appendChild(newCovid);
         moveCovid(newCovid);
-        covidInterval = Math.floor(Math.random()*2600) + 1900;
-        // console.log(covidInterval);
     }
-
-    function createBomb() {
-        let newBomb = document.createElement('img');
-        newBomb.src = 'img/bomb.png';
-        newBomb.classList.add('bomb');
-        newBomb.classList.add('bomb-transition');
-        newBomb.style.top = '50px';
-        newBomb.style.left = `${Math.floor(Math.random() * 330) + 30}px`;
-        playArea.appendChild(newBomb);
-        moveBomb(newBomb);
-        bombInterval = Math.floor(Math.random()*20000) + 30000;
-        // console.log(bombInterval);
-    }
-
-    function createStar() {
-        let newStar = document.createElement('img');
-        newStar.src = 'img/star.png';
-        newStar.classList.add('star');
-        newStar.classList.add('star-transition');
-        newStar.style.top = '50px';
-        newStar.style.left = `${Math.floor(Math.random() * 330) + 30}px`;
-        playArea.appendChild(newStar);
-        moveStar(newStar);
-        starInterval = Math.floor(Math.random()*30000) + 45000;
-        // console.log(starInterval);
-    }
-
 
     function moveCovid(covidElem) {
         let moveCovidInterval = setInterval(() => {
@@ -325,6 +241,49 @@ function main() {
         }, 30)
     }
 
+    //GIANT COVID
+    function createGiantCovid() {
+        let newGiantCovid = document.createElement('img');
+        newGiantCovid.src = 'img/covid_2.png';
+        newGiantCovid.classList.add('giant-covid');
+        newGiantCovid.classList.add('covid-transition');
+        newGiantCovid.style.top = '50px';
+        newGiantCovid.style.left = `${Math.floor(Math.random() * 330) + 30}px`;
+        playArea.appendChild(newGiantCovid);
+        moveGiantCovid(newGiantCovid);
+        console.log("giant created");
+    }
+
+    function moveGiantCovid(giantCovidElem) {
+        let moveGiantCovidInterval = setInterval(() => {
+            let xPosition = parseInt(window.getComputedStyle(giantCovidElem).getPropertyValue('top'));
+            if (xPosition >= 700) {
+                if (Array.from(giantCovidElem.classList).includes("dead-covid")) {
+                    giantCovidElem.remove();
+                    clearInterval(moveGiantCovidInterval)
+                } else {
+                    giantCovidElem.remove();
+                    clearInterval(moveGiantCovidInterval)
+                    handleGiantCollision();
+                }
+            } else {
+                giantCovidElem.style.top = `${xPosition + giantFallSpeed}px`;
+            }
+        }, 60)
+    }
+
+    //HEART
+    function createHeart() {
+        let newHeart = document.createElement('img');
+        newHeart.src = 'img/heart.png';
+        newHeart.classList.add('heart');
+        newHeart.classList.add('heart-transition');
+        newHeart.style.top = '50px';
+        newHeart.style.left = `${Math.floor(Math.random() * 330) + 30}px`;
+        playArea.appendChild(newHeart);
+        moveHeart(newHeart);
+    }
+
     function moveHeart(heartElem) {
         let moveHeartdInterval = setInterval(() => {
             let xPosition = parseInt(window.getComputedStyle(heartElem).getPropertyValue('top'));
@@ -342,21 +301,16 @@ function main() {
         }, 30)
     }
 
-    function moveBomb(bombElem) {
-        let moveBombInterval = setInterval(() => {
-            let xPosition = parseInt(window.getComputedStyle(bombElem).getPropertyValue('top'));
-            if (xPosition >= 700) {
-                if (Array.from(bombElem.classList).includes("dead-bomb")) {
-                    bombElem.remove();
-                    clearInterval(moveBombInterval)
-                } else {
-                    bombElem.remove();
-                    clearInterval(moveBombInterval)
-                }
-            } else {
-                bombElem.style.top = `${xPosition + fallSpeed}px`;
-            }
-        }, 30)
+    //STAR
+    function createStar() {
+        let newStar = document.createElement('img');
+        newStar.src = 'img/star.png';
+        newStar.classList.add('star');
+        newStar.classList.add('star-transition');
+        newStar.style.top = '50px';
+        newStar.style.left = `${Math.floor(Math.random() * 330) + 30}px`;
+        playArea.appendChild(newStar);
+        moveStar(newStar);
     }
 
     function moveStar(starElem) {
@@ -376,7 +330,36 @@ function main() {
         }, 30)
     }
 
+    //BOMB
+    function createBomb() {
+        let newBomb = document.createElement('img');
+        newBomb.src = 'img/bomb.png';
+        newBomb.classList.add('bomb');
+        newBomb.classList.add('bomb-transition');
+        newBomb.style.top = '50px';
+        newBomb.style.left = `${Math.floor(Math.random() * 330) + 30}px`;
+        playArea.appendChild(newBomb);
+        moveBomb(newBomb);
+    }
 
+    function moveBomb(bombElem) {
+        let moveBombInterval = setInterval(() => {
+            let xPosition = parseInt(window.getComputedStyle(bombElem).getPropertyValue('top'));
+            if (xPosition >= 700) {
+                if (Array.from(bombElem.classList).includes("dead-bomb")) {
+                    bombElem.remove();
+                    clearInterval(moveBombInterval)
+                } else {
+                    bombElem.remove();
+                    clearInterval(moveBombInterval)
+                }
+            } else {
+                bombElem.style.top = `${xPosition + fallSpeed}px`;
+            }
+        }, 30)
+    }
+
+    //CHECK COLLISION
     function checkSyringeCollision(syringe, elem) {
         let syringeLeft = parseInt(syringe.style.left);
         let syringeRight = 600 - syringeLeft;
@@ -384,8 +367,9 @@ function main() {
         let syringeBottom = syringeTop + 30; //syringe height
         let elemTop = parseInt(elem.style.top);
         let elemBottom = elemTop - 30;
-        let elemLeft = parseInt(elem.style.left);
-        let elemRight = 580 - elemLeft;
+        let elemLeft = parseInt(elem.style.left) - 20;
+        //let elemRight = 580 - elemLeft;
+        let elemRight = 555 - elemLeft;
 
         if (syringeTop > 5 && syringeTop + 40 <= elemTop) {
             if ((syringeLeft >= elemLeft && syringeRight >= elemRight)) {
@@ -398,15 +382,41 @@ function main() {
         }
     }
 
+    function checkSyringeCollisionGiant(syringe, elem) {
+        let syringeLeft = parseInt(syringe.style.left);
+        let syringeRight = 600 - syringeLeft;
+        let syringeTop = parseInt(syringe.style.top);
+        let syringeBottom = syringeTop + 30; //syringe height
+        let elemTop = parseInt(elem.style.top);
+        let elemBottom = elemTop - 30;
+        let elemLeft = parseInt(elem.style.left)-20;
+        let elemRight = 485 - elemLeft;
 
-    function gameOver() {
-        window.removeEventListener("keydown", letDoctorMove);
+        if (syringeTop > 5 && syringeTop + 40 <= elemTop) {
+            if ((syringeLeft >= elemLeft && syringeRight >= elemRight)) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    //GAME CONTROL
+    function clearingIntervals () {
         clearInterval(covidCreationInterval);
         clearInterval(heartCreationInterval);
         clearInterval(bombCreationInterval);
         clearInterval(starCreationInterval);
+        clearInterval(giantCovidCreationInterval);
+    }
+
+    function clearingPlayArea () {
         let covids = document.querySelectorAll(".covid");
         covids.forEach(covid => covid.remove());
+        let giantCovids = document.querySelectorAll(".giant-covid");
+        giantCovids.forEach(giantCovid => giantCovid.remove());
         let syringes = document.querySelectorAll(".syringe");
         syringes.forEach(syringe => syringe.remove());
         let hearts = document.querySelectorAll(".heart");
@@ -415,9 +425,77 @@ function main() {
         stars.forEach(star => star.remove());
         let bombs = document.querySelectorAll(".bomb");
         bombs.forEach(bomb => bomb.remove());
+    }
+
+    function settingIntervals () {
+        covidCreationInterval = setInterval(function () { createCovid() }, covidInterval);
+        heartCreationInterval = setInterval(function () {createHeart()}, heartInterval);
+        bombCreationInterval = setInterval(function () {createBomb()}, bombInterval);
+        starCreationInterval = setInterval(function () {createStar()}, starInterval);
+        giantCovidCreationInterval = setInterval(function () {createGiantCovid()}, giantCovidInterval);
+    }
+
+    function resumeGame (event) {
+        if(event.key === "Escape") {
+            startButton.style.display = 'none';
+            instructions.style.display = 'none';
+            window.addEventListener("keydown", letDoctorMove);
+            giantFallSpeed = 1;
+            fallSpeed = 2;
+            settingIntervals();
+            window.removeEventListener("keydown", resumeGame);
+        } else {       
+            startButton.style.display = 'none';
+            instructions.style.display = 'none';
+            window.addEventListener("keydown", letDoctorMove);
+            giantFallSpeed = 1;
+            fallSpeed = 2;
+            settingIntervals();
+            window.removeEventListener("keydown", resumeGame);
+        }
+
+    }
+
+    function endGame () {
+        window.removeEventListener("keydown", letDoctorMove);
+        startButton.style.display = 'none';
+        clearingIntervals();
+        clearingPlayArea();
+        handleHighscore();
+        instructions.innerHTML = `Game Over! Your final score is ${scoreCounter.innerText}!`;
+        instructions.style.display = "block";
+        instructions.style.color = "red";
+        startButton.style.display = "block";
+    }
+ 
+    function pauseGame (event) {
+        if (event.key === "Escape") {
+            event.preventDefault();
+            window.removeEventListener("keydown", letDoctorMove);
+            clearingIntervals();
+            giantFallSpeed = 0;
+            fallSpeed = 0;
+            let syringes = document.querySelectorAll(".syringe");
+            syringes.forEach(syringe => syringe.remove());
+            instructions.innerHTML = `Paused`;
+            instructions.style.display = "block";
+            instructions.style.color = "red";
+            instructions.insertAdjacentHTML("beforeend", `<br><button id="resume">Resume</button>` );      
+            instructions.insertAdjacentHTML("beforeend", `<br><button id="end">End</button>` );
+            const resumeBtn = document.querySelector("#resume");   
+            const endBtn = document.querySelector("#end");
+            resumeBtn.addEventListener('click', resumeGame);   
+            endBtn.addEventListener('click', endGame);
+            window.addEventListener("keydown", resumeGame);
+        }
+    }
+
+    function gameOver() {
+        window.removeEventListener("keydown", letDoctorMove);
+        clearingIntervals();
+        clearingPlayArea();
         handleHighscore();
         setTimeout(() => {
-
             instructions.innerHTML = `Game Over! The viruses made it to your Grandma. Your final score is ${scoreCounter.innerText}!`;
             instructions.style.display = "block";
             instructions.style.color = "red";
@@ -439,6 +517,7 @@ function main() {
         heartCreationInterval = setInterval(function () {createHeart()}, heartInterval);
         bombCreationInterval = setInterval(function () {createBomb()}, bombInterval);
         starCreationInterval = setInterval(function () {createStar()}, starInterval);
+        giantCovidCreationInterval = setInterval(function () {createGiantCovid()}, giantCovidInterval);
     }
 }
 window.addEventListener("load", main);
